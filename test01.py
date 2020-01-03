527e43f6b095a33aa067f4f60b2d754b2180f61c
Daryl Van Dyke <vandyke.geospatial@gmail.com>
2020-01-02 22:18:56 -0800

    Update test01.py

--- test01.py
+++ test01.py
@@ -8,6 +8,9 @@ def remove_control_characters(s):
     return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
 
 
+def serial_anticlobber(t = 0.11):
+    time.sleep(t)
+
 
 class sIO:
 
@@ -19,21 +22,37 @@ class sIO:
         
         self.sIO = serial.Serial(strCOM, timeout)
         print(self.sIO.name)
+
+        # # #
+        # local variables
+
+        self._strBuff = []        #  buffer for string ops
+        #
+        # # #
+
+        # init reporting
         self._ser_write(b'a')
-        time.sleep(0.01)
+        serial_anticlobber()
         self._ser_read()
-        time.sleep(0.01)
-        #self.cmd('a')
+        serial_anticlobber()
         
     def __enter__(self):
         return self
 
     # #
+    
+   
+    # # 
     def _ser_read(self):
         self.waiting = self.sIO.in_waiting
-        print( self.sIO.read(self.waiting) )
+        readBuf = self.sIO.read(self.waiting)
+        print(type(readBuf))
+        self._strBuff = readBuf.decode()
+        #print(self._strBuff)
+        print( readBuf.decode() )
 
     def _ser_write(self, strIn, fByte = True):
+        # TODO fix the byte testing
         if fByte:
             self.sIO.write(strIn)
         else:
@@ -41,14 +60,12 @@ class sIO:
     
 
     # #  User methods
-    def make_action(self, strIn):
+    def send(self, strIn):
         self._ser_read()
-        time.sleep(0.01)
+        serial_anticlobber()
         self._ser_write( strIn, fByte = True)
-        time.sleep(0.01)
+        serial_anticlobber()
         self._ser_read()
-
-
 
     def close(self):
         self.sIO.close()
@@ -57,8 +74,39 @@ class sIO:
         self.sIO.close()
 
 
-s = sIO()
+def boolNDLRrecord(lineRecord):
+    pass
 
-s._ser_write(b'a')
-s._ser_read()
+class pyNuDLR:
+    
+    def __init__(self):
+        self.sIO = sIO()
+
+    def a_pull_patterns(self):
+        #self.sIO.sIO.write(b'<b>')
+        self.sIO.send(b'<b>')
+        listPatterns = s.sIO._strBuff.split('\n')
+        print(type(listPatterns))
+        print(len(listPatterns))
+        for items in listPatterns:
+            if items[0] != '<':
+                pass
+            else:
+                print(items)
+            #if items[0] == '<':
+            #    print(items)
+        #
+#read(self.waiting)
+    
+s = pyNuDLR()
+s.a_pull_patterns()
+print("_________________")
+print(s.sIO._strBuff)
+
+
+
+#s = sIO()
+
+#s._ser_write(b'a')
+#s._ser_read()
 
